b19810408f1d907f3a6bd5bf1baf6d2f01317040
Carlos Eduardo L Lopes <carlos.el.lopes@gmail.com>
2012-11-20 19:33:00 -0200

    FAQ page added

---
+++ faq.php
@@ -0,0 +1,88 @@
+﻿<?php include '_header.php'; include '_submenu.html'; ?>
+
+                <section>
+
+                    <h2>Perguntas Frequentes</h2>
+
+                    <h3 class="abre">O que fazer quando eu encontrar um cão/gato em situação de necessidade?</h3>
+
+                    <p class="abre-conteudo">
+                    	Ao nos depararmos com um animal que precisa da nossa ajuda, temos que ter consciência de que se optarmos por ajudá-lo,
+						essa é uma opção nossa. Por isso, não podemos transferir as conseqüências dessa decisão para mais ninguém. Infelizmente,
+						hoje não podemos contar com uma atuação efetiva do Poder Público no problema relativo aos animais de rua. Por isso, cabe a nós,
+						pessoas que se importam com a grande massa de animais doentes e famintos vagando sem rumo pela cidade, fazer alguma coisa.
+						Embora haja algumas instituições na cidade que trabalham pela causa animal, elas sozinhas não podem mudar tudo.
+						É preciso que todos façam a sua parte – o que inclui assumir a responsabilidade de mudar o destino de um animal necessitado.<br />
+						Certamente, estaremos de prontidão para dar as instruções necessárias e ajudar a todos que quiserem ajudar os animais.
+						Mas veja: será um trabalho de parceria, e não de transferência do problema. Assim, podemos ajudar com a castração a baixo custo,
+						talvez com lar temporário (muitas vezes, é preciso pagar por isso) e, principalmente, na posterior doação do animal. Ou seja,
+						você vai precisar também por a mão na massa – mas garantimos que não há melhor sensação do que saber que você ajudou um ser vivo
+						a ter uma vida mais digna. Em parceria, podemos mudar drasticamente o cenário desta cidade, no que diz respeito aos animais de rua.
+                    </p>
+
+                    <h3 class="abre">Vocês têm abrigo?</h3>
+
+                    <p class="abre-conteudo">
+                    	Nós não temos abrigo.
+                    </p>
+
+                    <h3 class="abre">Porque esterilizar se consigo encontrar um bom lar para todos os filhotes?</h3>
+
+                    <p class="abre-conteudo">
+                    	E se a sua gata tiver 6 gatos? E se a sua cadela tiver 8 ou mais cães? Será mesmo que consegue encontrar famílias responsáveis
+						para todos? A triste realidade é que não há famílias suficientes para acolher todos os animais que precisam de um lar,
+						muito menos famílias que cuidem dos animais de forma verdadeiramente responsável. Muitos desses cães e gatos acabam acorrentados
+						ou esquecidos numa varanda, acabam abandonados nas ruas, são vítimas de abusos e maus-tratos ou adoecem e morrem de maneira
+						cruel.<br />
+						Mesmo que consiga encontrar um lar para todos os filhotes, será que a futura família será suficientemente responsável para evitar
+						que esses filhotes venham mais tarde a reproduzir-se? E quem evitará a reprodução dos filhotes desses filhotes? Vai estar lá para
+						garantir um bom lar para todos?<br />
+						Por outro lado, para muitas pessoas, um animal só é atraente e adorável enquanto é pequeno, ou enquanto não destrói o sofá da casa
+						nem faz xixi no tapete, ou enquanto não fica doente. Muitos animais são abandonados logo que ficam mais crescidos.<br />
+						Por outro lado ainda, para cada animal que se reproduz estará tirando a chance dos animais que já nasceram e
+						aguardam uma família.
+                    </p>
+
+					<h3 class="abre">Por que o gato não deve ter acesso à rua?</h3>
+
+                    <p class="abre-conteudo">
+                    	Os ancestrais selvagens dos gatos viviam à solta, mas isso foi há alguns milhares de anos e várias centenas de gerações atrás.
+						Esses ancestrais habitavam as regiões áridas do Oriente Médio.<br />
+						Existe ainda a crença de que o gato é um animal que precisa de "espaço e liberdade". Apesar disso, a expectativa de vida de um
+						gato que tem acesso à rua é de cinco anos, sendo que gatos que moram dentro de casa chegam a viver mais de vinte anos.
+						Nas ruas, há o grande perigo de atropelamentos, de maus tratos, de envenenamento, de roubo, de cachorros e de doenças.
+						Geralmente, quem já teve gatos soltos conta que o gato simplesmente "sumiu um dia". Esse "sumiço" foi muito provavelmente
+						uma morte trágica.<br />
+						Gatos soltos podem também entrar em brigas barulhentas com outros gatos à noite, comer ou arrancar as plantas dos vizinhos,
+						matar pássaros ou enterrar seus dejetos nos jardins alheios. Embora algumas pessoas - e alguns donos de gatos -
+						considerem esses problemas insignificantes, muitas outras os consideram gravíssimos. Se o seu gato entrar em uma briga,
+						as conseqüências serão piores do que o fato de acordar os vizinhos com os miados e berros. Os arranhões superficiais na
+						face ou no dorso do animal não são tão problemáticos. Mas ele pode ter ferimentos provocados por mordidas que se fecham rapidamente,
+						com sujeira e germes em seu interior, e que criam um abscesso doloroso vários dias depois. As mordidas durante as brigas também são
+						a forma principal de disseminação do vírus da imunodeficiência felina. Gatos não esterilizados que vagueiam pelas ruas também
+						contribuem para o aumento da população felina, um problema que lota os abrigos e resulta em milhões de animais "sacrificados"
+						anualmente.
+                    </p>
+
+					<h3 class="abre">Se eu esterilizar meus animais, eles não vão sentir falta de sexo?</h3>
+
+                    <p class="abre-conteudo">
+                    	Temos o hábito de “humanizar” os animais. Não raramente, ouvimos coisas como “ela (a cadela) é uma sem-vergonha – vive se oferecendo
+						para o macho”, “não quero castrar, pois eles precisam se divertir também”  ou mesmo “tadinha, ela tem direito de ser mãe
+						também/nossa, ela é uma mãe tão zelosa...". Isso mostra que muitas pessoas ainda acreditam que os bichos procuram uns aos outros
+						da mesma forma que os humanos ou tem uma relação com a maternidade como a dos humanos, o que não é verdade. O sexo para os animais
+						é algo puramente instintivo e com fins de reprodução, e sabemos que com os humanos, outros elementos entram em jogo. O mesmo vale
+						para a maternidade.<br />
+						Essas pessoas acabam esquecendo que depois de 2 meses, 10 “prazerzinhos” nascem – e, muito freqüentemente, sobra para as entidades
+						protetoras acudir e ajudar na doação. Isso quando não são simplesmente jogados fora, criando problemas para todo mundo e expondo os
+						filhotes a todo tipo de crueldade. E pior, tem gente que mesmo assim continua não querendo esterilizar o animal. <br />
+						Por tudo isso e pelos filhotinhos que a gente sabe que não terão chance, se você realmente está preocupado com o bem-estar de seu
+						animal, a melhor opção para todos – para ele, para você, para a sociedade – é a esterilização. Desta forma, você estará atuando de
+						forma efetiva nesta luta por um mundo melhor para todos.
+                    </p>
+
+					<a href="javascript:window.history.go(-1)" title="voltar" class="voltar">voltar</a>
+
+                </section>
+
+<?php include '_tiles.html'; include '_footer.php'; ?>
