49411755fe728e3485ea97ca54b449936534aea0
Carlos Eduardo L Lopes <carlos.el.lopes@gmail.com>
2012-11-22 10:33:27 -0200

    Added prestacao-de-contas page

---
+++ prestacao-de-contas.php
@@ -0,0 +1,16 @@
+﻿<?php include '_header.php'; include '_submenu.html'; ?>
+
+                <section>
+                    <h2>Prestação de contas</h2>
+
+                    <p class="abre-conteudo">
+                    	<a href="http://gaarcampinas.org/images/stories/relatGAAR_set10-maio11-web.pdf" target="_blank">Relatório - Set/2010 a Mai/2011</a><br />
+						<br />
+						<a href="http://gaarcampinas.org/images/stories/Relatorios/RELATORIO_GAAR_1sem2010.pdf" target="_blank">Relatório - Jan-Ago - 2010</a><br />
+                    </p>
+
+					<a href="javascript:window.history.go(-1)" title="voltar" class="voltar">voltar</a>
+
+                </section>
+
+<?php include '_tiles.html'; include '_footer.php'; ?>
