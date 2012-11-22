0c2888f9d4c3b96ac58dd6314221400b510b7bfd
Carlos Eduardo L Lopes <carlos.el.lopes@gmail.com>
2012-11-22 10:26:57 -0200

    Articles page added

---
+++ artigos.php
@@ -0,0 +1,23 @@
+﻿<?php include '_header.php'; include '_submenu.html'; ?>
+
+                <section>
+
+                    <h2>Artigos</h2>
+
+                    <p>Esta seção disponibiliza artigos relacionados a causa animal.</p>
+
+                    <p class="abre-conteudo">
+                    	<a href="http://gaarcampinas.org/arquivos/Folha%20Online%20-%20Bichos%20-%20Ani...pdf" target="_blank">- Animal de estimação deve ser preparado para a chegada do bebê</a><br />
+						<br />
+						<a href="http://gaarcampinas.org/arquivos/Folha%20Online%20-%20Bichos%20-%20Pro...pdf" target="_blank">- Protetores de animais têm regras rígidas para doação</a><br />
+						<br />
+						<a href="http://gaarcampinas.org/arquivos/Gravidas_e_pets.pdf" target="_blank">- Gravidas, Crianças e Animais numa boa</a><br />
+						<br />
+						<a href="http://gaarcampinas.org/arquivos/Veja.pdf" target="_blank">- Os amigos abandonados</a>
+                    </p>
+
+                    <a href="javascript:window.history.go(-1)" title="voltar" class="voltar">voltar</a>
+
+                </section>
+
+<?php include '_tiles.html'; include '_footer.php'; ?>
