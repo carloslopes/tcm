a2e47cd1ffc8d95d7f439ec4e5262d82339d079e
Carlos Eduardo L Lopes <carlos.el.lopes@gmail.com>
2012-11-22 10:29:46 -0200

    Added colaboradores page

---
+++ colaboradores.php
@@ -0,0 +1,28 @@
+﻿<?php include '_header.php'; include '_submenu.html'; ?>
+
+                <section>
+
+                    <h2>Colaboradores</h2>
+
+					<p>Nesta seção estão relacionados alguns dos colaboradores que contribuem periodicamente com o nosso grupo.</p>
+
+                    <h3 class="abre">Barão Pet</h3>
+
+                    <p>
+						R. Benedito Alves Aranha, 127<br />
+						Tel. (19) 3289-9410<br />
+						Em frente ao Supermercado Barão - Barão Geraldo - Campinas
+                    </p>
+
+					<h3 class="abre">DOG STREET</h3>
+
+                    <p>
+						Av Albino José Barbosa de Oliveira 1297 - Barão Geraldo<br />
+						(19) 3289-7212
+                    </p>
+
+                    <a href="javascript:window.history.go(-1)" title="voltar" class="voltar">voltar</a>
+
+                </section>
+
+<?php include '_tiles.html'; include '_footer.php'; ?>
