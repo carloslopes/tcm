b8589fc23016177f4fcb5a20fb06c61d7e5df618
Carlos Eduardo L. Lopes <carlos.el.lopes@gmail.com>
2012-12-07 10:54:29 -0200

    Added better search result format

--- busca.php
+++ busca.php
@@ -21,15 +21,44 @@
 
 ?>
 
-  <section>
+  <section class="resultado-busca">
     <?php if(empty($animals)) { ?>
     <h2>Sua busca não obteve resultados</h2>
     <?php } else { ?>
     <h2>Resultado da busca</h2>
 
-    <?php foreach($animals as $animal) { ?>
-      <p><a href="<?php $animal->show_path() ?>"><?php echo $animal->name ?></a></p>
-    <?php }} ?>
+    <ul>
+      <?php foreach($animals as $animal) { ?>
+      <?php $pictures = $animal->pictures() ?>
+      <li>
+        <a href="<?php $animal->show_path() ?>" title="<?php $animal->name ?>">
+          <img src="<?php echo $pictures[0] ?>" height="140px" width="200px" alt="<?php $animal->name ?>" title="<?php $animal->name ?>" />
+
+          <h3 class="nome-animal"><?php echo $animal->name ?></h3>
+
+          <table border="0" cellspacing="0" cellpadding="0" width="740">
+            <tr>
+              <td><h4>Espécie: </h4></td>
+              <td><?php echo $animal->specie() ?></td>
+              <td><h4>Raça: </h4></td>
+              <td><?php echo $animal->breed() ?></td>
+              <td rowspan="2"><h4>Comportamento: </h4></td>
+              <td rowspan="2"><?php echo $animal->description ?></td>
+            </tr>
+            <tr>
+              <td><h4>Cor: </h4></td>
+              <td><?php echo $animal->color() ?></td>
+              <td><h4>Idade aproximada: </h4></td>
+              <td><?php show($animal->age) ?></td>
+            </tr>
+          </table>
+        </a>
+      </li>
+      <?php } ?>
+    </ul>
+    <?php } ?>
+
+    <a href="javascript:window.history.go(-1)" title="voltar" class="voltar">voltar</a>
   </section>
 
 <?php include '_tiles.html'; include '_footer.php'; ?>
